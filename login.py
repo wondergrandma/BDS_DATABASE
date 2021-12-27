222a2dc031c316692a5bcc9a4a1dd67ad400fee5
wondergrandma <oliverbielik@gmail.com>
2021-12-27 19:12:01 +0100

    pridanie loginu zatial nefunguje

--- login.py
+++ login.py
@@ -1,42 +1,36 @@
 import logging
+import psycopg2
 from tkinter import *
 import bcrypt
-import hashlib
+
+#Premené pre pripojenie aplikácie k PG admin
+DB_HOST = "localhost"
+DB_NAME = "projekt"
+DB_USER = "postgres"
+DB_PASS = "postgres"
 
 class Login:
 
-    """mail = ""
-    password = ""
-    
-    def __init__(self, mail, password):
-        self.mail = mail
+    email = ""
+    password = u""
+
+    def __init__(self, email, password):
+        self.email = email
         self.password = password
-        self.hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt())"""
-
-    def loginScreen(self, window):
+        self.hashed = bcrypt.hashpw(password, bcrypt.gensalt())
         
-        window.title("Postgres Database")
-        window.geometry("500x250")
+    def comparePasswords(self):
+        conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+        cur = conn.cursor()
 
-        frame = LabelFrame(window, padx=50, borderwidth=0)
-        frame.pack(padx=10,pady=10)
+        cur.execute("SELECT pwd FROM \"user\" WHERE mail = %s ", (self.email,))
+        hashed = cur.fetchall()
+        result = hashed[0][0]
 
-        label_mail_1 = Label(frame, text="Enter email:")
-        label_mail_2 = Label(frame, text="Enter password:")
-        label_mail_1.grid(row=0, column=0)
-        label_mail_2.grid(row=1,column=0)
+        conn.commit()
+        cur.close()
 
-        t1 = Entry(frame, width=10)
-        t2 = Entry(frame, width=10)
-        t1.grid(row=0, column=1)
-        t2.grid(row=1,column=1)
-
-        def hashPwd():
-            get_pwd = str(t2.get())
-            hash_pwd = str(get_pwd.hashlib.sha256(bytes).hexdigest())
-            return hash_pwd
-
-            
-        
-        login_button1 = Button(window, text="LOG IN", command = hashPwd)
-        login_button1.pack()
+        if bcrypt.checkpw(self.password.encode(), result):
+            return True
+        else:
+            return False    
--- main_gui.py
+++ main_gui.py
@@ -2,13 +2,51 @@ from tkinter import *
 from tree_view import TreeView
 from login import Login
 
-
 window = Tk()
 tree_w = TreeView()
-login = Login()
+#login = Login()
 
-tree_w.viewDatabase(window)
+def loginScreen():
+    global email_entry, pwd_entry
+    window.title("Postgres Database")
+    window.geometry("500x250")
+
+    frame = LabelFrame(window, padx=50, borderwidth=0)
+    frame.pack(padx=10,pady=10)
+
+    label_mail_1 = Label(frame, text="Enter email:")
+    label_mail_2 = Label(frame, text="Enter password:")
+    label_mail_1.grid(row=0, column=0)
+    label_mail_2.grid(row=1,column=0)
+
+    email_entry = Entry(frame, width=10)
+    pwd_entry = Entry(frame, width=10)
+    email_entry.grid(row=0, column=1)
+    pwd_entry.grid(row=1,column=1)
+
+    def comparePasswords():
+        global email
+        email = email_entry.get()
+        global password
+        password = pwd_entry.get()
+        global lg
+        lg = Login(email, password)
+        
+        if (lg.comparePasswords() == True):
+            print("SUCCESS")
+        else:
+            print("NO SECCESS")
+
+    login_button1 = Button(window, text="LOG IN", command = comparePasswords)
+    login_button1.pack()
+
+#tree_w.viewDatabase(window)
 
 #login.loginScreen(window)
 
+
+loginScreen()
 window.mainloop()
+
+#oliverbielik@gmail.com
+#ahoj
